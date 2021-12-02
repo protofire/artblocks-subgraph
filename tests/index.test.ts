441e127a5b8df95f864eef5b2891c5dc72d46829
Andrés Fernández <andresavena19@gmail.com>
2021-12-01 20:11:41 -0500

    feat: test OperatorOwner

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,9 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { ADDRESS_ZERO } from "@protofire/subgraph-toolkit"
 import { clearStore, test, assert } from "matchstick-as/assembly/index"
-import { Transfer, Mint, Approval } from "../generated/artblocks/artblocks"
-import { handleTest, handleMint, handleTransfer, handleApproval } from "../src/mappings"
-import { tests as testsModule, transactions } from "../src/modules"
+import { Transfer, Mint, Approval, ApprovalForAll } from "../generated/artblocks/artblocks"
+import { handleTest, handleMint, handleTransfer, handleApproval, handleApprovalForAll } from "../src/mappings"
+import { accounts, tests as testsModule, transactions } from "../src/modules"
 
 test("testing matschtick",
 	() => {
@@ -215,7 +215,7 @@ test("handleBurn",
 test("handleApproval",
 	() => {
 		let owner = Address.fromString("0x7b7cc10852f215bcea3e684ef584eb2b7c24b8f7")
-		let approved = Address.fromString(ADDRESS_ZERO)
+		let approved = Address.fromString("0x9b9cc10852f215bcea3e684ef584eb2b7c24b8f7")
 		let tokenId = BigInt.fromI32(666)
 
 		let event = changetype<Approval>(testsModule.helpers.events.getNewEvent(
@@ -254,6 +254,53 @@ test("handleApproval",
 		let entityTokenId = tokenId.toHexString()
 		assert.fieldEquals("Token", entityTokenId, "owner", ownerId)
 		assert.fieldEquals("Token", entityTokenId, "approval", approvedId)
+
+		clearStore()
+	}
+)
+
+test("handleApprovalForAll",
+	() => {
+		let owner = Address.fromString("0x7b7cc10852f215bcea3e684ef584eb2b7c24b8f7")
+		let operator = Address.fromString("0x9b9cc10852f215bcea3e684ef584eb2b7c24b8f7")
+		let approved = false
+
+		let event = changetype<ApprovalForAll>(testsModule.helpers.events.getNewEvent(
+			[
+				testsModule.helpers.params.getAddress("owner", owner),
+				testsModule.helpers.params.getAddress("operator", operator),
+				testsModule.helpers.params.getBoolean("approved", approved),
+			]
+		))
+		handleApprovalForAll(event)
+
+		// check block
+		let blockId = event.block.number.toString()
+		assert.fieldEquals("Block", blockId, "timestamp", event.block.timestamp.toString())
+		assert.fieldEquals("Block", blockId, "number", blockId)
+
+		// check meta
+		let txId = event.transaction.hash.toHexString()
+		assert.fieldEquals("TransactionMeta", txId, "block", blockId)
+		assert.fieldEquals("TransactionMeta", txId, "hash", txId)
+		assert.fieldEquals("TransactionMeta", txId, "from", event.transaction.from.toHexString())
+		assert.fieldEquals("TransactionMeta", txId, "gasLimit", event.transaction.gasLimit.toString())
+		assert.fieldEquals("TransactionMeta", txId, "gasPrice", event.transaction.gasPrice.toString())
+
+		// check owner
+		// TODO: assert should support types
+		let ownerId = owner.toHexString()
+		assert.fieldEquals("Account", ownerId, "address", owner.toHex())
+
+		// check approved
+		let operatorId = operator.toHexString()
+		assert.fieldEquals("Account", operatorId, "address", operator.toHex())
+
+		// check OperatorOwner
+		let operatorOwnerId = accounts.helpers.getOperatorOwnerId(ownerId, operatorId)
+		assert.fieldEquals("OperatorOwner", operatorOwnerId, "owner", ownerId)
+		assert.fieldEquals("OperatorOwner", operatorOwnerId, "operator", operatorId)
+		assert.fieldEquals("OperatorOwner", operatorOwnerId, "approved", approved.toString())
 
 		clearStore()
 	}
