0b57dafd9444a94ad4be3cfdc805a3849f66554e
Andrés Fernández <andresavena19@gmail.com>
2021-12-01 19:21:25 -0500

    feat: handleBurn test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -163,3 +163,51 @@ test("handleMintTransfer",
 		clearStore()
 	}
 )
+
+test("handleBurn",
+	() => {
+		let from = Address.fromString("0x7b7cc10852f215bcea3e684ef584eb2b7c24b8f7")
+		let to = Address.fromString(ADDRESS_ZERO)
+		let tokenId = BigInt.fromI32(666)
+
+		let event = changetype<Transfer>(testsModule.helpers.events.getNewEvent(
+			[
+				testsModule.helpers.params.getAddress("from", from),
+				testsModule.helpers.params.getAddress("to", to),
+				testsModule.helpers.params.getBigInt("tokenId", tokenId)
+			]
+		))
+		handleTransfer(event)
+
+		// check block
+		let blockId = event.block.number.toString()
+		assert.fieldEquals("Block", blockId, "timestamp", event.block.timestamp.toString())
+		assert.fieldEquals("Block", blockId, "number", blockId)
+
+		// check meta
+		let txId = event.transaction.hash.toHexString()
+		assert.fieldEquals("TransactionMeta", txId, "block", blockId)
+		assert.fieldEquals("TransactionMeta", txId, "hash", txId)
+		assert.fieldEquals("TransactionMeta", txId, "from", event.transaction.from.toHexString())
+		assert.fieldEquals("TransactionMeta", txId, "gasLimit", event.transaction.gasLimit.toString())
+		assert.fieldEquals("TransactionMeta", txId, "gasPrice", event.transaction.gasPrice.toString())
+
+		// check seller
+		let buyerId = to.toHexString()
+		let sellerId = from.toHexString()
+		assert.fieldEquals("Account", sellerId, "id", sellerId)
+
+		// check token
+		let entityTokenId = tokenId.toHexString()
+		assert.fieldEquals("Token", entityTokenId, "owner", buyerId)
+
+		// check transfer
+		let transferId = transactions.helpers.getNewTransactionId(sellerId, buyerId, event.block.timestamp)
+		assert.fieldEquals("Burn", transferId, "from", sellerId)
+		assert.fieldEquals("Burn", transferId, "to", buyerId)
+		assert.fieldEquals("Burn", transferId, "token", entityTokenId)
+		assert.fieldEquals("Burn", transferId, "block", blockId)
+
+		clearStore()
+	}
+)
